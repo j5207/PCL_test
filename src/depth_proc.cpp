d20db9af1da098608ff683d90645c6201f2d3525
jhan53 <jhan53@jhu.edu>
2018-07-01 21:45:11 -0400

    bug

--- src/depth_proc.cpp
+++ src/depth_proc.cpp
@@ -13,6 +13,15 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl_ros/transforms.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/segmentation/region_growing_rgb.h>
+
 
 
 
@@ -52,25 +61,65 @@ class TestKinect
     {
       pcl::PCLPointCloud2::Ptr pc_in(new pcl::PCLPointCloud2());
       pcl::PCLPointCloud2::Ptr pc_out(new pcl::PCLPointCloud2());
-      pcl_conversions::toPCL(*ros_cloud_in, *pc_in);
+      pcl::PCLPointCloud2::Ptr final_out(new pcl::PCLPointCloud2());
+
+      pcl::PointCloud<pcl::PointXYZ>::Ptr temp_pc(new pcl::PointCloud<pcl::PointXYZ>);
+      pcl::PointCloud<pcl::PointXYZ>::Ptr temp_out(new pcl::PointCloud<pcl::PointXYZ>);
+      pcl::PointCloud<pcl::PointXYZ>::Ptr temp_haha(new pcl::PointCloud<pcl::PointXYZ>);
+
+      pcl::PointCloud <pcl::PointXYZRGB>::Ptr rgb_pc (new pcl::PointCloud <pcl::PointXYZRGB>);
+      
       pcl::PointCloud<pcl::PointXYZ>::Ptr filter_pc(new pcl::PointCloud<pcl::PointXYZ>);
+      pcl_conversions::toPCL(*ros_cloud_in, *pc_in);
       pcl::fromPCLPointCloud2(*pc_in, *filter_pc);
 
       // Create the VoxelGrid filtering object
-      // pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-      // sor.setInputCloud(pc_in);
-      // sor.setLeafSize(0.1f, 0.1f, 0.1f);
-      // sor.filter(*pc_out);
+      pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+      sor.setInputCloud(pc_in);
+      sor.setLeafSize(0.01f, 0.01f, 0.01f);
+      sor.filter(*pc_out);
+
+      std::cout << "heng" << std::endl;
+      // Convert pointcloud2 to template pcl
+      pcl::fromPCLPointCloud2(*pc_out, *rgb_pc);
+
+      std::cout << "kuku" << std::endl;
+
+
+      pcl::search::Search <pcl::PointXYZRGB>::Ptr tree = boost::shared_ptr<pcl::search::Search<pcl::PointXYZRGB> > (new pcl::search::KdTree<pcl::PointXYZRGB>);
+
+
+      pcl::IndicesPtr indices (new std::vector <int>);
+      pcl::PassThrough<pcl::PointXYZRGB> pass;
+      pass.setInputCloud (rgb_pc);
+      pass.setFilterFieldName ("z");
+      pass.setFilterLimits (0.0, 1.0);
+      pass.filter (*indices);
+
+      pcl::RegionGrowingRGB<pcl::PointXYZRGB> reg;
+      reg.setInputCloud (rgb_pc);
+      reg.setIndices (indices);
+      reg.setSearchMethod (tree);
+      reg.setDistanceThreshold (10);
+      reg.setPointColorThreshold (6);
+      reg.setRegionColorThreshold (5);
+      reg.setMinClusterSize (600);
+
+      std::vector <pcl::PointIndices> clusters;
+      reg.extract (clusters);
+
+      pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
 
       // Create StatisticalOutlierRemoval
-      pcl::StatisticalOutlierRemoval<pcl::PCLPointCloud2> sor;
-      sor.setInputCloud(pc_in);
-      sor.setMeanK(50);
-      sor.setStddevMulThresh(1.0);
-      sor.filter(*pc_out);
+      // pcl::StatisticalOutlierRemoval<pcl::PCLPointCloud2> sor;
+      // sor.setInputCloud(pc_in);
+      // sor.setMeanK(50);
+      // sor.setStddevMulThresh(1.0);
+      // sor.filter(*pc_out);
       // Convert back to sensor msg
+      pcl::toPCLPointCloud2(*colored_cloud, *final_out);
       sensor_msgs::PointCloud2::Ptr msg_out(new sensor_msgs::PointCloud2());
-      pcl_conversions::fromPCL(*pc_out, *msg_out);
+      pcl_conversions::fromPCL(*final_out, *msg_out);
       pub_cloud.publish(msg_out);
 
 
@@ -98,4 +147,5 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "depth_proc");
     TestKinect test;
     ros::spin();
+
 }
